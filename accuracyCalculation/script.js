b3419ec58198ee692c3107381424154364017f77
leinigoe <leinigoe@gmail.com>
2023-12-28 15:03:42 -0800

    Added basic functionality

--- accuracyCalculation/script.js
+++ accuracyCalculation/script.js
@@ -1,1 +1,18 @@
-console.log("test");
+window.addEventListener ("DOMContentLoaded", function() {
+    console.log("hello");
+    calcAcc();
+
+    var inputs = document.getElementsByTagName('input');
+    for (var i in inputs) {
+        switch (inputs[i].type) {
+            case "number":
+                inputs[i].addEventListener("input", calcAcc);
+                break;
+        }
+    }
+
+})
+function calcAcc() {
+    accuracy = document.getElementById("acc").value;
+    console.log(accuracy);
+}
